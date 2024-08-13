337b48db4b1c84adff811d2684b435ff965fc4e0
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-13 17:28:07 +0530

    Time: 4 ms (56.25%), Space: 45.5 MB (9.9%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+public class Solution {
+    private HashMap<Integer, Integer> valueCount = new HashMap<>();
+    private int maxCount = 0;
+
+    public int[] findMode(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+
+
+        inorderTraversal(root);
+
+
+        List<Integer> modesList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : valueCount.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                modesList.add(entry.getKey());
+            }
+        }
+
+        int[] modesArray = new int[modesList.size()];
+        for (int i = 0; i < modesList.size(); i++) {
+            modesArray[i] = modesList.get(i);
+        }
+
+        return modesArray;
+    }
+
+    private void inorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraversal(node.left);
+
+        int currentValue = node.val;
+        int currentCount = valueCount.getOrDefault(currentValue, 0) + 1;
+        valueCount.put(currentValue, currentCount);
+        maxCount = Math.max(maxCount, currentCount);
+
+        inorderTraversal(node.right);
+    }
+}
