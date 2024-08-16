ad3b24f96de89d045e9a5664713461ad5e50173b
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-16 20:09:34 +0530

    Time: 12 ms (98.74%), Space: 47.1 MB (36.93%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -1,34 +1,29 @@
-class Solution {
-    // static int mod=(int)(1e9+7);
-    // static int ways(int n,int k,int dp[][]){
-    // if (n == 0)
-    //         return k == 0 ? 1 : 0;
-    //     if (k < 0) 
-    //         return 0;
-    //     if (dp[n][k] != -1) 
-    //         return dp[n][k];
-    //     int res = 0;
-    //     for (int i = 0; i<=Math.min(k,n-1); i++) {
-    //         res = (res + ways(n - 1, k - i, dp)) % mod;
-    //     }
-    //     dp[n][k] = res;
-    //     return res;
+public class Solution {
+    int M = 1000000007;
 
-    // }
     public int kInversePairs(int n, int k) {
-       int mod = 1000000007;
-        int[][] dp = new int[n + 1][k + 1];
-        dp[0][0] = 1;
-        
+        int[][] t = new int[n + 1][k + 1];
+
+        // t[i][j] = total number of arrays having (1 to i) and exactly j inversions
+
+        // for j = 0, t[i][0] = 1
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = 1;
+        }
+
+        // O(n*k)
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int m = 0; m <= j; m++) {
-                    if (m <= i - 1) {
-                        dp[i][j] = (dp[i][j] + dp[i - 1][j - m]) % mod;
-                    }
+            long cumSum = 1;
+            for (int j = 1; j <= k; j++) {
+
+                cumSum += t[i - 1][j];
+                if (j >= i) {
+                    cumSum -= t[i - 1][j - i];
                 }
+                t[i][j] = (int) (cumSum % M);
             }
         }
-        return dp[n][k];
+
+        return t[n][k];
     }
 }
