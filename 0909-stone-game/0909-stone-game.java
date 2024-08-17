efa44eadf77ef9ed646a87509c2ccc951581f10e
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-17 17:45:08 +0530

    Time: 0 ms (100%), Space: 41.1 MB (79.22%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int a=0;
+        int b=0;
+        int res=1,ln=piles.length;
+        for(int i=0;i<ln;i++){
+            if(res%2!=0){
+                a+=Math.max(piles[i],piles[ln-i-1]);
+            }
+            else{
+                b+=Math.min(piles[i],piles[ln-i-1]);
+            }
+            res++;
+        }
+        if(b<a){
+            return true;
+        } 
+        return false;
+    }
+}
