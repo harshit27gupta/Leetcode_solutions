a09d32f522de102e7093840455656b7277c2ee28
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-17 12:13:08 +0530

    Time: 29 ms (89.26%), Space: 40.5 MB (50.14%) - LeetHub

---
+++ 0897-prime-palindrome/0897-prime-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static  boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static  int primePalindrome(int n) {
+        while (true) {
+            if (n == reverse(n) && isPrime(n)) {
+                return n;
+            }
+            n++;
+            if (10_000_000 < n && n < 100_000_000) {
+                n = 100_000_000;
+            }
+        }
+    }
+
+    public static int reverse(int x) {
+        int r = 0;
+        while (x > 0) {
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+        return r;
+    }
+}
+
+    
