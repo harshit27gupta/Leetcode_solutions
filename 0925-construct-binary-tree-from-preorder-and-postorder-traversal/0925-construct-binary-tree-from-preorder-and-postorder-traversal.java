8ca8ad10bdc7e6ff41d4621c3a1d843c3fa94ac0
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-18 11:57:32 +0530

    Time: 1 ms (59.31%), Space: 43.7 MB (12.91%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder)
+    {
+        if(preorder == null || postorder == null || preorder.length == 0 || postorder.length == 0)  return null;
+        
+        TreeNode root = new TreeNode(preorder[0]);
+        int mid = 0;
+        
+        if(preorder.length == 1)    return root;
+        for(int i = 0; i < postorder.length; i++)
+        {
+            if(preorder[1] == postorder[i])
+            {
+                mid = i;
+                break;
+            }
+        }
+        
+        root.left = constructFromPrePost(Arrays.copyOfRange(preorder, 1, 1 + mid + 1), Arrays.copyOfRange(postorder, 0, mid + 1));
+        
+        root.right = constructFromPrePost(Arrays.copyOfRange(preorder, 1 + mid + 1, preorder.length),Arrays.copyOfRange(postorder, mid + 1, postorder.length - 1));
+        return root;
+    }
+}
