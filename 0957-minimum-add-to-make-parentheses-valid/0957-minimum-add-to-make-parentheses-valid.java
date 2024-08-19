f009c7756f05f6da14f7d96f46e0040d3616c503
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-08-19 11:08:52 +0530

    Time: 2 ms (29.04%), Space: 41.6 MB (25.43%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+        Stack s = new Stack<>();
+        int c = 0;
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i) == '('){
+                s.add(S.charAt(i));
+            }
+            else{
+                if(!s.isEmpty()){
+                    s.pop();
+                }
+                else{
+                    c++;
+                }
+            }
+        }
+        c = c + s.size();
+        return c;
+    }
+}
