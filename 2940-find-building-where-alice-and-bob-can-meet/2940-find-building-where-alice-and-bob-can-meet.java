4a17740e15489eb36b3d82e5bba86f7d67390ddf
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-12-22 13:01:25 +0530

    Time: 60 ms (26.92%), Space: 71.6 MB (84.62%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
@@ -1,68 +1,78 @@
 class Solution {
-    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        List<Pair<Integer, Integer>> monoStack = new ArrayList<>();
-        int[] result = new int[queries.length];
-        Arrays.fill(result, -1);
-        List<List<Pair<Integer, Integer>>> newQueries = new ArrayList<>(
-            heights.length
-        );
+    static class SparseTable {
+        private int[][] st; // Sparse table
+        private int[] log;  // Logarithm values
+        private int n;      // Array size
 
-        for (int i = 0; i < heights.length; i++) {
-            newQueries.add(new ArrayList<>());
-        }
+        // Constructor: Build the Sparse Table
+        public SparseTable(int[] arr) {
+            this.n = arr.length;
+            int maxLog = (int) (Math.log(n) / Math.log(2)) + 1;
 
-        for (int i = 0; i < queries.length; i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            if (a > b) {
-                int temp = a;
-                a = b;
-                b = temp;
+            // Initialize Sparse Table and log array
+            st = new int[n][maxLog];
+            log = new int[n + 1];
+
+            // Compute log values for fast access
+            for (int i = 2; i <= n; i++) {
+                log[i] = log[i / 2] + 1;
             }
-            if (heights[b] > heights[a] || a == b) {
-                result[i] = b;
-            } else {
-                newQueries.get(b).add(new Pair<>(heights[a], i));
-            }
-        }
 
-        for (int i = heights.length - 1; i >= 0; i--) {
-            int monoStackSize = monoStack.size();
-            for (Pair<Integer, Integer> pair : newQueries.get(i)) {
-                int position = search(pair.getKey(), monoStack);
-                if (position < monoStackSize && position >= 0) {
-                    result[pair.getValue()] = monoStack
-                        .get(position)
-                        .getValue();
+            // Fill the first column of Sparse Table
+            for (int i = 0; i < n; i++) {
+                st[i][0] = arr[i];
+            }
+
+            // Fill the rest of the Sparse Table
+            for (int j = 1; (1 << j) <= n; j++) {
+                for (int i = 0; i + (1 << j) - 1 < n; i++) {
+                    st[i][j] = Math.max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
                 }
             }
-
-            while (
-                !monoStack.isEmpty() &&
-                monoStack.get(monoStack.size() - 1).getKey() <= heights[i]
-            ) {
-                monoStack.remove(monoStack.size() - 1);
-            }
-
-            monoStack.add(new Pair<>(heights[i], i));
         }
 
-        return result;
+        // Query the maximum in the range [L, R]
+        public int query(int L, int R) {
+            int j = log[R - L + 1];
+            return Math.max(st[L][j], st[R - (1 << j) + 1][j]);
+        }
     }
-
-    private int search(int height, List<Pair<Integer, Integer>> monoStack) {
-        int left = 0;
-        int right = monoStack.size() - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (monoStack.get(mid).getKey() > height) {
-                ans = Math.max(ans, mid);
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        SparseTable st = new SparseTable(heights);
+        int[] res = new int[queries.length];
+        int n = heights.length;
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0], r = queries[i][1];
+            if (l > r) {
+                int temp = l;
+                l = r;
+                r = temp;
             }
+            if (l == r) {
+                res[i] = l;
+                continue;
+            }
+            if (heights[r] > heights[l]) {
+                res[i] = r;
+                continue;
+            }
+            int maxHeight = Math.max(heights[l], heights[r]);
+            int left = r + 1, right = n - 1, mid;
+            int leftmostIndex = -1;
+            while (left <= right) {
+                mid = left + (right - left) / 2;
+                int maxInRange = st.query(left, mid);
+
+                if (maxInRange > maxHeight) {
+                    leftmostIndex = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            res[i] = leftmostIndex;
         }
-        return ans;
+
+        return res;
     }
 }
