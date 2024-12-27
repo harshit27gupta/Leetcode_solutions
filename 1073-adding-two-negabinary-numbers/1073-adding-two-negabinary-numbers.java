4959ea62812d4d5a39d2984eb03a747be33337ff
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2024-12-27 17:34:46 +0530

    Time: 2 ms (65.91%), Space: 44.9 MB (52.27%) - LeetHub

---
+++ 1073-adding-two-negabinary-numbers/1073-adding-two-negabinary-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int[] addNegabinary(int[] arr1, int[] arr2) {
+    int i1 = arr1.length - 1, i2 = arr2.length - 1, carry = 0;
+    List<Integer> resultList = new ArrayList<Integer>();
+    while (i1 >= 0 || i2 >= 0 || carry != 0) {
+        int n1 = i1 >= 0 ? arr1[i1--] : 0;
+        int n2 = i2 >= 0 ? arr2[i2--] : 0;
+        int sum = n1 + n2 + carry;
+		int result = sum & 1;
+        carry = -1 * (sum >> 1) ;
+		resultList.add(0, result);        
+    }
+    int beginIndex = 0;
+    while (beginIndex < resultList.size() && resultList.get(beginIndex) == 0)
+        beginIndex++;	
+    if (beginIndex == resultList.size())
+        return new int[]{0};
+    int resultArray[] = new int[resultList.size() - beginIndex];
+    for (int i = 0; i < resultArray.length; i++)
+        resultArray[i] = resultList.get(i + beginIndex);
+    return resultArray;
+}
+}
