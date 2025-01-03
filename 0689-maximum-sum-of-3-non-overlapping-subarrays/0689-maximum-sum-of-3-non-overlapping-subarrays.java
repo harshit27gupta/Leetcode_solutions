c4eebecd35d2019d46d63144b01a789241591a9d
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2025-01-03 18:35:43 +0530

    Time: 4 ms (72.76%), Space: 46.9 MB (37.67%) - LeetHub

---
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int bestSingleStart = 0;
+        int[] bestDoubleStart = { 0, k };
+        int[] bestTripleStart = { 0, k, k * 2 };
+        int singleWindowSum = 0;
+        for(int i = 0; i < k; i++) {
+            singleWindowSum += nums[i];
+        }
+        int doubleWindowSum = 0;
+        for(int i = k; i < k * 2; i++) {
+            doubleWindowSum += nums[i];
+        }
+        int tripleWindowSum = 0;
+        for(int i = k * 2; i < k * 3; i++) {
+            tripleWindowSum += nums[i];
+        }
+        int bestSingleSum = singleWindowSum;
+        int bestDoubleSum = singleWindowSum + doubleWindowSum;
+        int bestTripleSum = singleWindowSum + doubleWindowSum + tripleWindowSum;
+        int singleStartIndex = 1;
+        int doubleStartIndex = k + 1;
+        int tripleStartIndex = k * 2 + 1;
+        while (tripleStartIndex <= nums.length - k) {
+            singleWindowSum = singleWindowSum - nums[singleStartIndex - 1] + nums[singleStartIndex + k - 1];
+            doubleWindowSum = doubleWindowSum - nums[doubleStartIndex - 1] + nums[doubleStartIndex + k - 1];
+            tripleWindowSum = tripleWindowSum - nums[tripleStartIndex - 1] + nums[tripleStartIndex + k - 1];
+            if (singleWindowSum > bestSingleSum) {
+                bestSingleStart = singleStartIndex;
+                bestSingleSum = singleWindowSum;
+            }
+
+            if (doubleWindowSum + bestSingleSum > bestDoubleSum) {
+                bestDoubleStart[0] = bestSingleStart;
+                bestDoubleStart[1] = doubleStartIndex;
+                bestDoubleSum = doubleWindowSum + bestSingleSum;
+            }
+
+            if (tripleWindowSum + bestDoubleSum > bestTripleSum) {
+                bestTripleStart[0] = bestDoubleStart[0];
+                bestTripleStart[1] = bestDoubleStart[1];
+                bestTripleStart[2] = tripleStartIndex;
+                bestTripleSum = tripleWindowSum + bestDoubleSum;
+            }
+            singleStartIndex += 1;
+            doubleStartIndex += 1;
+            tripleStartIndex += 1;
+        }
+        return bestTripleStart;
+    }
+}
