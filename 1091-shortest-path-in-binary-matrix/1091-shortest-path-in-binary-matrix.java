7fc68579d6b91f212b2a55db5392e2b31b34af0f
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2025-01-07 13:06:03 +0530

    Time: 136 ms (5%), Space: 45.7 MB (39.37%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,4 +1,14 @@
 class Solution {
+    class Triplet{
+        int x;
+        int y;
+        int cost;
+        public Triplet(int x,int y,int cost){
+            this.x=x;
+            this.y=y;
+            this.cost=cost;
+        }
+    }
     static int dirc[][]={{0,1},{1,1},{0,-1},{1,-1},{-1,1},{-1,-1},{-1,0},{1,0}};
     static int find(int grid[][],int i,int j,int n,int m,int cnt){
 if(i==n-1 && j==m-1)
@@ -21,7 +31,30 @@ return ans;
       int m=grid[0].length;
       if(grid[0][0]==1 || grid[n-1][m-1]==1)
       return -1;
-      int ans= find(grid,0,0,n,m,0);
-      return ans==10000?-1:ans;
+PriorityQueue<Triplet> pq=new PriorityQueue<>((a,b)->{
+return Integer.compare(a.cost,b.cost);
+});
+pq.offer(new Triplet(0,0,1));
+boolean visited[][]=new boolean[n][m];
+while(!pq.isEmpty()){
+    Triplet node=pq.poll();
+    if(node.x==n-1 && node.y==m-1)
+    return node.cost;
+    if(visited[node.x][node.y])
+    continue;
+    visited[node.x][node.y]=true;
+    for(int dir[]:dirc){
+    int x=dir[0];
+    int y=dir[1];
+    int newx=node.x+x;
+    int newy=node.y+y;
+    if(newx<n && newy<m && newx>=0 && newy>=0 && grid[newx][newy]==0)
+  pq.offer(new Triplet(newx,newy,node.cost+1));
+}
+}
+
+
+
+     return -1;
     }
 }
