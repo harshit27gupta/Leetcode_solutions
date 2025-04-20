1cd46e07765b2c45cd1a3f79369c669047dc0fbe
harshit27gupta <129065536+harshit27gupta@users.noreply.github.com>
2025-04-20 22:47:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3399-smallest-substring-with-identical-characters-ii/3399-smallest-substring-with-identical-characters-ii.java
+++ 3399-smallest-substring-with-identical-characters-ii/3399-smallest-substring-with-identical-characters-ii.java
@@ -1,36 +1,34 @@
 class Solution {
-   boolean check(String s, int numOps, int mid, char startChar){
-    for(int i = 0; i < s.length(); ++i){
-        if(startChar == s.charAt(i)) numOps--;
-        startChar = (startChar == '0')?'1':'0';
-    }
-    return (numOps >= 0);
-}
-
-boolean isValid(String s, int numOps, int mid){
-    if(mid == 1) return check(s, numOps, mid, '1') || check(s, numOps, mid, '0');
-    int count = 0;
-    for(int i = 0, last = -1; i < s.length(); ++i){
-        if(s.charAt(i) == last) count++; 
-        else{
-            numOps -= count/(mid+1);
-            last = s.charAt(i);
-            count = 1;
+    static boolean isValid(String s,int numOps,int mid){
+        char prev='a';
+    int cnt=0;
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i)==prev)
+        cnt++;
+        else {
+            int flips=cnt/(mid+1);
+            numOps-=flips;
+        prev=s.charAt(i);
+        cnt=1;
         }
     }
-    if(count > mid) numOps -= count/(mid+1);
-    return (numOps >= 0);
-}
-    
-int minLength(String s, int numOps) {
-    int start = 1, end = s.length(), mid, ans = s.length();
-    while(start <= end){
-        mid = start + (end - start)/2;
-        if(isValid(s, numOps, mid)){
-            ans = mid;
-            end = mid - 1;
-        }else start = mid + 1;
+    if(cnt>mid)
+    numOps-=cnt/(mid+1);
+    return (numOps>=0);
     }
-    return ans;
-}
+    public int minLength(String s, int numOps) {
+        int low=1;
+        int high=s.length();
+        int min_len=s.length();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isValid(s,numOps,mid)){
+                min_len=mid;
+                high=mid-1;
+            }
+            else
+            low=mid+1;
+        }
+        return min_len;
+    }
 }
