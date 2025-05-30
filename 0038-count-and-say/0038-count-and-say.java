a758879ad4987857a0ef6fcc9f3ff7e6e20a6021
Harshit Gupta <iib2022015@iiita.ac.in>
2025-05-30 11:15:05 +0530

    Time: 10 ms (19.31%), Space: 44 MB (19.91%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,21 +1,33 @@
 class Solution {
+  static String encode(String str){
+    char ch=str.charAt(0);
+    int cnt=0;
+    StringBuilder sb=new StringBuilder();
+for(int i=0;i<str.length();i++){
+if(str.charAt(i)==ch)
+cnt++;
+else{
+sb.append(cnt+""+(ch));
+cnt=0;
+ch=str.charAt(i);
+i--;
+}
+}
+if(cnt>0)
+    sb.append(cnt+""+(ch));
+return sb.toString();
+  }
     public String countAndSay(int n) {
-        if(n==1)
+       if(n==1)
         return "1";
+        if(n==2)
+        return "11";
+        if(n==3)
+        return "21";
+        String ans="21";
+        for(int i=4;i<=n;i++)
+        ans=encode(ans);
+        return ans;
 
-        String s=countAndSay(n-1);
-
-        int c=0;
-        StringBuilder ans=new StringBuilder();
-
-        for(int i=0;i<s.length();i++){
-            c++;
-            if(i==s.length()-1 || s.charAt(i)!=s.charAt(i+1)){
-                ans.append(c).append(s.charAt(i));
-                c=0;
-            }
-        }
-        return ans.toString();
-        
     }
 }
