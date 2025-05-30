ca3d777e67b31df66ecb834d5393711f346ee647
Harshit Gupta <iib2022015@iiita.ac.in>
2025-05-30 19:45:38 +0530

    Time: 0 ms (100%), Space: 43 MB (87.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1=-1;
-        int ind2=-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind1=i;
-                break;
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find the first index 'i' such that nums[i] < nums[i+1] from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If such index is found, find the index 'j' to swap with
+        if (i >= 0) {
+            int j = n - 1;
+
+            // Find the next greater element than nums[i] from the end
+            while (nums[j] <= nums[i]) {
+                j--;
             }
+
+            // Swap the elements at i and j
+            swap(nums, i, j);
         }
-        if(ind1==-1){
-            Arrays.sort(nums);
-        }
-        else{
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]>nums[ind1]){
-                ind2=i;
-                break;
-            }
-        }
-        int tmp=nums[ind1];
-        nums[ind1]=nums[ind2];
-        nums[ind2]=tmp;
-         reverse(nums, ind1 + 1, nums.length - 1);
-        }
-    }
-        private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
-        }
+
+        // Step 3: Reverse the subarray from i+1 to end to get the next smallest lexicographic order
+        reverse(nums, i + 1, n - 1);
     }
 
+    // Utility to swap elements at two indices
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Utility to reverse elements from index i to j
+    static void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 }
