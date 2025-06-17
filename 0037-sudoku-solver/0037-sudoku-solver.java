29c2a78104f881f904275eb15b00e4e1ed3c5052
Harshit Gupta <iib2022015@iiita.ac.in>
2025-06-17 10:45:04 +0530

    Time: 59 ms (93.92%), Space: 40.9 MB (85.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,53 +1,42 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        int[][] f=new int[9][10];
-        int[][] row=new int[9][10];
-        int[][] col=new int[9][10];
-        int cell=0;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-             cell=((i/3)*3)+j/3;
-             if(board[i][j]!='.'){
-             f[cell][(int)(board[i][j]-'0')]=1;
-             row[i][(int)(board[i][j]-'0')]=1;
-             col[j][(int)(board[i][j]-'0')]=1;
-            }}
-        }
-        get(f,board,0,0,row,col);
-        return;
+    boolean[][] rowUsed = new boolean[9][10];
+    boolean[][] colUsed = new boolean[9][10];
+    boolean[][] boxUsed = new boolean[9][10];
 
-    }
-    static boolean get(int[][] f,char[][] board, int i, int j, int[][] row, int[][] col ){
-        if(i==9)return true;
-        if(board[i][j]!='.'){
-            if(j==8){
-                return get(f,board,i+1,0,row,col);
-            }else{
-                return get(f,board,i,j+1,row,col);
-            }
-        }else{
-            boolean t=false;
-            int cell=((i/3)*3) + j/3;
-            for(int k=1;k<=9;k++){
-                if(f[cell][k]==0 && row[i][k]==0 && col[j][k]==0){
-                    f[cell][k]=1;
-                    row[i][k]=1;
-                    col[j][k]=1;
-                    board[i][j]=(char) (k + '0');
-                    if(j==8){
-                        t=get(f,board,i+1,0,row,col);
-                    }else{
-                        t=get(f,board,i,j+1,row,col);
-                    }
-                    if(t)return true;
-                    f[cell][k]=0;
-                    row[i][k]=0;
-                    col[j][k]=0;
-                    board[i][j]='.';
+    public void solveSudoku(char[][] board) {
+        // Initialize usage from pre-filled board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int d = board[i][j] - '0';
+                    rowUsed[i][d] = true;
+                    colUsed[j][d] = true;
+                    boxUsed[(i / 3) * 3 + (j / 3)][d] = true;
                 }
             }
         }
-       return false;
+        solve(board, 0, 0);
     }
 
+    boolean solve(char[][] board, int i, int j) {
+        if (i == 9) return true;
+        if (j == 9) return solve(board, i + 1, 0);
+
+        if (board[i][j] != '.') return solve(board, i, j + 1);
+
+        for (int d = 1; d <= 9; d++) {
+            int boxId = (i / 3) * 3 + (j / 3);
+            if (!rowUsed[i][d] && !colUsed[j][d] && !boxUsed[boxId][d]) {
+                board[i][j] = (char) ('0' + d);
+                rowUsed[i][d] = colUsed[j][d] = boxUsed[boxId][d] = true;
+
+                if (solve(board, i, j + 1)) return true;
+
+                // backtrack
+                board[i][j] = '.';
+                rowUsed[i][d] = colUsed[j][d] = boxUsed[boxId][d] = false;
+            }
+        }
+        return false;
+    }
 }
