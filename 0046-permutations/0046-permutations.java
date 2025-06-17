399226b9aaadfa23cfa448f322d4fade9c1c7907
Harshit Gupta <iib2022015@iiita.ac.in>
2025-06-17 09:47:58 +0530

    Time: 2 ms (39.14%), Space: 45 MB (10.8%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,32 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length == 1) {
-            List<Integer> singleList = new ArrayList<>();
-            singleList.add(nums[0]);
-            res.add(singleList);
-            return res;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            int[] remainingNums = new int[nums.length - 1];
-            int index = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    remainingNums[index] = nums[j];
-                    index++;
-                }
-            }
-            
-            List<List<Integer>> perms = permute(remainingNums);
-            for (List<Integer> p : perms) {
-                p.add(n);
-            }
-            
-            res.addAll(perms);
-        }
-        
-        return res;        
+        List<List<Integer>> ans=new ArrayList<>();
+       Set<Integer> st=new HashSet<>();
+        List<Integer> store=new ArrayList<>();
+        ways(ans,st,nums,store);
+        return ans;
+    }
+    static void ways(List<List<Integer>> ans,Set<Integer> st,int[] nums,List<Integer> store){
+if(st.size()==nums.length){
+    ans.add(new ArrayList<>(store));
+    return; 
+}
+for(int i=0;i<nums.length;i++){
+    if(st.contains(i))
+    continue;
+    st.add(i);
+    store.add(nums[i]);
+    ways(ans,st,nums,store);
+    st.remove(i);
+    store.remove(store.size()-1);
+}
     }
 }
