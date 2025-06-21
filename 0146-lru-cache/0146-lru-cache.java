91898a265dd20ba406396cbb24e01afa8a1dd1fd
Harshit Gupta <iib2022015@iiita.ac.in>
2025-06-21 09:28:53 +0530

    Time: 86 ms (7.81%), Space: 121.8 MB (18.52%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,105 +1,63 @@
+import java.util.*;
+
 class LRUCache {
-    class Node{
-        int key;
-        int value;
+    PriorityQueue<Pair> pq;
+    HashMap<Integer, Integer> map;
+    HashMap<Integer, Integer> keyToTime; 
+    int size;
+    int time;
 
-        Node prev;
-        Node next;
-
-        Node(int key, int value){
-            this.key= key;
-            this.value= value;
-        }
-    }
-
-    public Node[] map;
-    public int count, capacity;
-    public Node head, tail;
-    
     public LRUCache(int capacity) {
-        
-        this.capacity= capacity;
-        count= 0;
-        
-        map= new Node[10_000+1];
-        
-        head= new Node(0,0);
-        tail= new Node(0,0);
-        
-        head.next= tail;
-        tail.prev= head;
-        
-        head.prev= null;
-        tail.next= null;
+        pq = new PriorityQueue<>((a, b) -> a.time - b.time); 
+        map = new HashMap<>();
+        keyToTime = new HashMap<>();
+        size = capacity;
+        time = 0;
     }
-    
-    public void deleteNode(Node node){
-        node.prev.next= node.next;
-        node.next.prev= node.prev;       
-        
-        return;
-    }
-    
-    public void addToHead(Node node){
-        node.next= head.next;
-        node.next.prev= node;
-        node.prev= head;
-        
-        head.next= node;      
-        
-        return;
-    }
-    
+
     public int get(int key) {
-        
-        if( map[key] != null ){
-            
-            Node node= map[key];
-            
-            int nodeVal= node.value;
-            
-            deleteNode(node);
-            
-            addToHead(node);
-            
-            return nodeVal;
+        if (map.containsKey(key)) {
+            int val = map.get(key);
+            keyToTime.put(key, time); 
+            pq.offer(new Pair(key, val, time));
+            time++;
+            return val;
         }
-        else
-            return -1;
+        return -1;
     }
-    
+
     public void put(int key, int value) {
-        
-        if(map[key] != null){
-            
-            Node node= map[key];
-            
-            node.value= value;
-            
-            deleteNode(node);
-            
-            addToHead(node);
-            
+        if (map.containsKey(key)) {
+            map.put(key, value);
         } else {
-            
-            Node node= new Node(key,value);
-            
-            map[key]= node;
-            
-            if(count < capacity){
-                count++;
-                addToHead(node);
-            } 
-            else {
-                
-                map[tail.prev.key]= null;
-                deleteNode(tail.prev);
-                
-                addToHead(node);
+            if (map.size() >= size) {
+                removeLRU();
+            }
+            map.put(key, value);
+        }
+        keyToTime.put(key, time); 
+        pq.offer(new Pair(key, value, time));
+        time++;
+    }
+
+    private void removeLRU() {
+        while (!pq.isEmpty()) {
+            Pair node = pq.poll();
+            if (map.containsKey(node.key) && keyToTime.get(node.key) == node.time) {
+                map.remove(node.key);
+                keyToTime.remove(node.key);
+                break;
             }
         }
-        
-        return;
     }
-    
+
+    class Pair {
+        int key, value, time;
+
+        public Pair(int key, int value, int time) {
+            this.key = key;
+            this.value = value;
+            this.time = time;
+        }
+    }
 }
