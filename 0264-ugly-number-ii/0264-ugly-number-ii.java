a5bb8189e77575c00befa3321c6799c04c66fefb
Harshit Gupta <iib2022015@iiita.ac.in>
2025-06-21 11:10:43 +0530

    Time: 52 ms (24.5%), Space: 44.9 MB (20.6%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,19 +1,23 @@
-public class Solution {
+class Solution {
     public int nthUglyNumber(int n) {
-        int[] ugly = new int[n];
-        ugly[0] = 1;
-        int index2 = 0, index3 = 0, index5 = 0;
-        int factor2 = 2, factor3 = 3, factor5 = 5;
-        for(int i=1;i<n;i++){
-            int min = Math.min(Math.min(factor2,factor3),factor5);
-            ugly[i] = min;
-            if(factor2 == min)
-                factor2 = 2*ugly[++index2];
-            if(factor3 == min)
-                factor3 = 3*ugly[++index3];
-            if(factor5 == min)
-                factor5 = 5*ugly[++index5];
+        int[] primes = {2, 3, 5};
+        PriorityQueue<Long> uglyHeap = new PriorityQueue<>();
+        HashSet<Long> visited = new HashSet<>();
+        
+        uglyHeap.add(1L);
+        visited.add(1L);
+        
+        long curr = 1L;
+        for (int i = 0; i < n; i++) {
+            curr = uglyHeap.poll();
+            for (int prime : primes) {
+                long new_ugly = curr * prime;
+                if (!visited.contains(new_ugly)) {
+                    uglyHeap.add(new_ugly);
+                    visited.add(new_ugly);
+                }
+            }
         }
-        return ugly[n-1];
+        return (int)curr;
     }
 }
