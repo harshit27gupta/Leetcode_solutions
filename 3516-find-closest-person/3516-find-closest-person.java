0d959ac7993dfc552aa3164194bacb9663a9333e
Harshit Gupta <iib2022015@iiita.ac.in>
2025-09-04 10:19:27 +0530

    Time: 0 ms (100%), Space: 41.3 MB (20.21%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int dist1=Math.abs(x-z);
+        int dist2=Math.abs(y-z);
+        return (dist1>=dist2?(dist1==dist2?0:2):1);
+    }
+}
