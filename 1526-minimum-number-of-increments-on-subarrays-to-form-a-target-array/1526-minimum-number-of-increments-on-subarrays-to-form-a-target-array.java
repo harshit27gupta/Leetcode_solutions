5779f9dc92eec8e84ba3603c60918ab4f9971b87
Harshit Gupta <iib2022015@iiita.ac.in>
2025-10-30 10:21:28 +0530

    Time: 3 ms (100%), Space: 56.6 MB (81.13%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int ans=0,prev=0;
+        for(int i=0; i<target.length; i++) {
+            if(target[i] > prev) {
+                ans+=(target[i]-prev);
+            }
+            prev=target[i];
+        }
+        return ans;
+    }
+}
