074d080e760ef2a21b94eb3b21cf12aab7701dec
Harshit Gupta <iib2022015@iiita.ac.in>
2025-12-08 09:40:37 +0530

    Time: 7 ms (88.94%), Space: 42 MB (59.5%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countTriples(int n) {
+        int ans = 0;
+        for(int i = 3; i <= 3 * n / 4; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int sum = i*i + j*j;
+                int k = (int)Math.sqrt(sum);
+                if(k <= n) {
+                    if(k*k == sum) ans += 2;
+                } else break;
+            }
+        }   
+        return ans;
+    }
+}
